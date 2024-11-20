a93c071fbc6ee6398376103b4182a5021d78a6c3
YoussefMAllam <s-youssef.allam@zewailcity.edu.eg>
2024-11-20 23:21:07 +0200

    AA

--- process.c
+++ process.c
@@ -2,15 +2,30 @@
 
 /* Modify this file as needed*/
 int remainingtime;
+int starttime;
+int processedtime=0;
 
+bool running;
 int main(int agrc, char * argv[])
 {
     initClk();
+    
+    
+    
+    //When Process is Interrupted
     
     //TODO it needs to get the remaining time from somewhere
     //remainingtime = ??;
     while (remainingtime > 0)
     {
+        if(running){
+            processedtime=processedtime+getClk()-starttime;
+        }
+        else if(!running){
+            //Wait for signal
+            starttime=getClk();
+        }
+
         // remainingtime = ??;
     }
     
